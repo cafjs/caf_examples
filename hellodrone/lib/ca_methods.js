7ee8e77383443f7fa8fddc2b42689ffe6472974e
Antonio Lain <antlai@cafjs.com>
2013-06-25 22:10:34 -0700

    Add properties

--- hellodrone/lib/ca_methods.js
+++ hellodrone/lib/ca_methods.js
@@ -60,6 +60,96 @@ exports.methods = {
         this.$.iot.removeIoT(droneId);
         cb(null, "ok");
     },
+
+/* System properties are:
+ *
+ *  'control:euler_angle_max' (between 0 and 0.52, i.e., 30 degrees or PI/6).
+ *
+ *  'control:altitud_max'  in millimeters
+ *
+ *  'control:control_vz_max' Max vertical speed  in milimeters per second
+ * (max 2000)
+ *
+ *  'control:control_yaw' Max yaw speed in radians per second (max 6.11 rad/s
+ * or 350 degrees per second)
+ *
+ *  'control:outdoor' true to enable wind estimator
+ *
+ *  'control:flight_without_shell'  true if the outdoor hull is on.
+ *
+ *  'video:video_codec' set to 'H264_720P_CODEC' for live stream 720P
+ * otherwise 360P.
+ *
+ */
+    'setProperty' : function(droneId, name, value, cb) {
+        var map = this.$.iot.getIoT(droneId);
+        var fromCloud = map && map.fromCloud;
+        if (fromCloud) {
+            fromCloud[name] = value;
+            cb(null, "ok");
+        } else {
+            cb("Drone id not found:" + droneId);
+        }
+    },
+
+/*
+ *  'command' is a string representing a JSON serialized operation of the form:
+ *
+ *  Array.<{when: <number>, relative: <boolean>, op: <string>,
+ *          args: Array.<JSONSerializableObject>}>
+ *
+ * 'when' is UTC time (in miliseconds since 1/1/1970) or offset to perform the
+ *  task.
+ *
+ * 'relative' is whether 'when' is wall clock time or an offset from the start
+ *  of the previous task.
+ *
+ * 'op' is one of:
+ *      'takeoff'  - no args
+ *
+ *      'land'     -no args
+ *
+ *      'emergency'  - no args -  Drone stops by cutting off the engine
+ *
+ *      'stop' - no args- Drone hovers in place
+ *
+ *      'move' -with arguments:
+ *                 [roll, pitch, gaz, yaw]
+ *               all of them are floating point numbers [-1.0...1.0]
+ *  representing a relative value to a maximum threshold property:
+ *
+ *               'roll'  is left/right tilt with maximum angle defined with
+ *  property control:euler_angle_max (between 0 and 0.52, i.e., 30 degrees or
+ *  PI/6).
+ *               'pitch' is front/back tilt with maximum angle defined with
+ *  property 'control:euler_angle_max'.
+ *               'gaz' is vertical speed with maximum speed defined with
+ *  property 'control:control_vz_max' (going up is positive)
+ *               'yaw' is angular speed (clockwise is positive) with maximum
+ *  angular speed defined with property 'control:control_yaw'
+ *
+ *      'blink' -with arguments:
+ *                  [ledAnimation, rate, duration]
+ *
+ *       and 'ledAnimation' is any of 'blinkGreenRed', 'blinkGreen', 'blinkRed',
+ * 'blinkOrange', 'snakeGreenRed', 'fire', 'standard', 'red', 'green',
+ * 'redSnake','blank', 'rightMissile', 'leftMissile', 'doubleMissile',
+ * 'frontLeftGreenOthersRed', 'frontRightGreenOthersRed',
+ * 'rearRightGreenOthersRed','rearLeftGreenOthersRed', 'leftGreenRightRed',
+ * 'leftRedRightGreen','blinkStandard'
+ *            'rate' is #blinks per second
+ *            'duration' is length of animation in seconds
+ *
+ *      'video' -with arguments:
+ *                 [IP address, port, bottomCamera]
+ *       to stream video using a tcp socket with destination <IP, port>
+ *       or stop sending video if no arguments.
+ *                 'bottomCamera' is true if we control the ground camera.
+ *
+ *
+ * The sequence of tasks in 'command' should be sorted by starting time.
+ *
+ */
     'doCommand': function(droneId, command, cb) {
         this.$.iot.addCommand(droneId, command);
         cb(null, "ok");
