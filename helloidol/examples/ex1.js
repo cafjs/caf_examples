738ea610b22aaee2e44f895efa854962ea291f00
Antonio Lain <antlai@cafjs.com>
2014-04-29 00:53:17 -0700

    Adding resource polling

--- helloidol/examples/ex1.js
+++ helloidol/examples/ex1.js
@@ -2,7 +2,7 @@ var cli = require('caf_cli');
 var conduit = require('caf_conduit');
 var async = require('async');
 var REPO_PREFIX='https://region-a.geo-1.objects.hpcloudsvc.com/v1/87595414183311/CloudAssistant/';
-var MY_KEY='e9372328-6985-4652-8aea-c8966bb86c6d';
+var MY_KEY=process.env['MY_KEY'];
 
 
 var log = {
@@ -23,10 +23,17 @@ var log = {
     }
 };
 
+var cbObject = {
+    'dispatcher' : function(notification) {
+        console.log("NOTIF: got " + JSON.stringify(notification));
+    }
+};
 var session = new cli.Session({url:"http://helloidol.vcap.me:3000/ca/antonio_xx",
-                               password: "pleasechange",
-//                               log: log,
-                               disableBackChannel:true})
+                               cbObject : cbObject,
+                               password: "pleasechange"
+                               //,
+                               //log: log
+                              })
 
 
 var IDOL_APIs = ['detectlanguage', 'explodecontainer',
@@ -46,6 +53,13 @@ c = c.explodecontainer({'url': REPO_PREFIX + 'foo.tar'}, null,  'c1')
 
 
 async.series([
+                 function(cb) {
+                     session.remoteInvoke('autoMode',[true], cb);
+                 },
+                 function(cb) {
+                     session.remoteInvoke('addInput',['i1', REPO_PREFIX +
+                                                      'foo.tar'], cb);
+                 },
                  function(cb) {
                      session.remoteInvoke('addKey',[MY_KEY], cb);
                  },
--- helloidol/lib/ca_methods.js
+++ helloidol/lib/ca_methods.js
@@ -20,28 +20,30 @@ var utils = caf.myutils;
 
 var conduit = require('caf_conduit');
 
+var MAX_NUM_NOTIF = 5;
 
 var methods = exports.methods = {
     '__ca_init__' : function(cb) {
         this.state.opStr = null;
         this.scratch.op = null;
         this.state.acc = {};
+        this.scratch.inputs = {};
+        this.state.autoOn = false;
         cb(null);
     },
     '__ca_resume__' : function(cp, cb) {
         this.state = (cp && cp.state) || {};
         if (typeof this.state.opStr === 'string') {
             var op = conduit.parse(this.state.opStr);
-            this.scratch.op = op.__behavior__(utils.cloneAndMix(this.$.idol, methods));
+            this.scratch.op = op.__behavior__(utils.cloneAndMix(this.$.idol,
+                                                                methods));
         }
+        this.scratch.inputs = {};
         cb(null);
     },
     '__ca_pulse__' : function(cb) {
-        var self = this;
-        if (this.scratch.op) {
-// DEBUG...
-//            this.state.acc = {};
-//            this.scratch.op.__fold__(this.state.acc, cb);
+        if (this.state.autoOn && this.scratch.op) {
+            this.forceRefresh(cb);
         } else {
             cb(null);
         }
@@ -57,7 +59,8 @@ var methods = exports.methods = {
     'newOp': function(opStr, cb) {
         this.state.opStr = opStr;
         var op = conduit.parse(opStr);
-        this.scratch.op = op.__behavior__(utils.cloneAndMix(this.$.idol, methods));
+        this.scratch.op = op.__behavior__(utils.cloneAndMix(this.$.idol,
+                                                            methods));
         this.state.acc = {};
         this.scratch.op.__fold__(this.state.acc, cb);
     },
@@ -72,9 +75,64 @@ var methods = exports.methods = {
     // State is {acc:Object, opStr:string}
     'getState' : function(cb) {
         cb(null, this.state);
+    },
+
+    // Managing inputs
+    'addInput' : function(alias, url, cb) {
+        this.$.pull.addResource(alias, url, 'updatedFile');
+        cb(null, 'ok');
+    },
+    'removeInput' : function(alias, cb) {
+        this.$.pull.removeResource(alias);
+        cb(null, 'ok');
+    },
+    'listInputs' : function(cb) {
+        var all = this.$.pull.listResources();
+        cb(null, {inputs: all, autoOn: this.state.autoOn});
+    },
+    'forceRefresh' : function(cb) {
+        // check files for changes
+        var self = this;
+        var inputs = this.$.pull.listResources();
+        inputs.forEach(function(alias) {
+                              self.$.pull.refreshResource(alias);
+                          });
+        cb(null);
+    },
+    'autoMode' : function(autoOn, cb) {
+        this.state.autoOn = autoOn;
+        cb(null);
+    },
+    'updatedFile' : function(alias, fileName, version, cb) {
+        var self = this;
+        console.log('Got updated file notification: alias:' + alias +
+                    ' fileName:' + fileName + ' version:' + version);
+        if (this.scratch.inputs[alias] &&
+            this.scratch.inputs[alias].version === version) {
+            console.log('No Change for ' + fileName);
+            cb(null);  // next message
+        } else {
+            if (this.scratch.op) {
+                var cb0 = function(err, val) {
+                    if (err) {
+                        cb(err, val);
+                    } else {
+                        if (!val.error) { // TODO: handle app errors
+                        // Do not retry until file changes (or explicit call)
+                            self.scratch.inputs[alias] = {'version' : version};
+                        }
+                        self.$.session.boundQueue(MAX_NUM_NOTIF, 'default');
+                        self.$.session.notify(val, 'default');
+                        cb(err, val);
+                    }
+                };
+                this.executeOp(cb0);
+            } else {
+                cb(null);
+            }
+        }
     }
 };
-
 
 caf.init(__dirname, {});
 
